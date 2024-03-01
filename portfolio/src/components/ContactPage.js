198a739eb8b7cb3304d72cc3177aa51dd1e3f78d
michalbieniek02 <michalbieniek02@gmail.com>
2024-03-02 00:51:43 +0100

    navbar remake done

---
+++ portfolio/src/components/ContactPage.js
@@ -0,0 +1,11 @@
+//github.com/michalbieniek02
+//https://www.linkedin.com/in/michalbieniek02/
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>ContactPage</div>
+  )
+}
+
+export default ContactPage
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom';
 import resume_pdf from '../assets/cv.pdf'
 const Navbar = () => {
   return (
-    <nav className="bg-white fixed w-full z-50">
+    <nav className="bg-black fixed w-full z-50">
       <div className="container mx-auto flex items-center justify-between p-4">
         <div className="text-purple-500 font-bold text-xl">
-          <Link to="/">Portfolio</Link>
+          <Link to="/"><span className='text-purple-500'>Full</span><span className='text-yellow-500'>stack</span> <span className='text-purple-300'>developer</span></Link>
         </div>
         <label htmlFor="check" className="cursor-pointer lg:hidden">
           <i className="ri-menu-line text-2xl"></i>
         </label>
         <input type="checkbox" name="check" id="check" className="hidden" />
         <ul className="hidden lg:flex gap-4">
-          <li><Link to="/" className="hover:text-purple-500">Home</Link></li>
+          <li><Link to="/" className="hover:text-purple-500 text-yellow-500 font-semibold">Home</Link></li>
           <li>
-            <Link to={resume_pdf} className="hover:text-purple-500" 
+            <Link to={resume_pdf} className="hover:text-purple-500 text-yellow-500 font-semibold" 
                     target='_blank' rel='noopener noreferrer'>
                   Resume
             </Link>
           </li>
-          <li><Link to="/portfolio" className="hover:text-purple-500">Portfolio</Link></li>
-          <li><Link to="/contact" className="hover:text-purple-500">Contact</Link></li>
+          <li><Link to="/portfolio" className="hover:text-purple-500 text-yellow-500 font-semibold">Portfolio</Link></li>
+          <li><Link to="/contact" className="hover:text-purple-500 text-yellow-500 font-semibold">Contact</Link></li>
         </ul>
       </div>
     </nav>
